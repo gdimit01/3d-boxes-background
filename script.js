50706295366514848608f5c72371cdd3f99bf552
Georgi Dimitrov <gd353@kent.ac.uk>
2023-04-28 21:05:18 +0100

    updated js

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click", () => boxesContainer.classList.toggle("big"));
 
 function createBoxes() {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
+  for (let i = 0; i < 4; i++) //so this controls the number of boxes columns 
+  {
+    for (let j = 0; j < 4; j++) //so this controls the number of boxes rows
+    {
       const box = document.createElement("div");
       box.classList.add("box");
-      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`; //so this controls the position of the boxes
       boxesContainer.appendChild(box);
     }
   }
